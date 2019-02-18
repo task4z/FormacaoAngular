11f01f6c19a73d97ceb63b49218b38d3a2cfec26
unknown <andreafl90@gmail.com>
2019-02-18 21:15:40 +0000

    coisas meio feitas+

--- routing/projects/params/src/app/product-detail/product-detail.component.ts
+++ routing/projects/params/src/app/product-detail/product-detail.component.ts
@@ -5,7 +5,8 @@ import { ActivatedRoute } from '@angular/router';
   selector: 'app-product-detail',
   template: `
   <h4 class='product'>PRODUCT DETAIL COMPONENT</h4>
-  <h3 class='product'>Com ID: {{productId}}</h3>
+  <h3 class='product' *ngIf="productId">Com ID: {{productId}}</h3>
+  <h3 class='product' *ngIf="productCategory">Com categoria: {{productCategory}}</h3>
 `,
 styles: ['.product{background:yellow;}']
   ,
@@ -13,12 +14,14 @@ styles: ['.product{background:yellow;}']
 export class ProductDetailComponent implements OnInit {
 
   productId: string;
+  productCategory: string;
 
   constructor(private route: ActivatedRoute) {
     this.productId = route.snapshot.paramMap.get('id');
     //this.productId = route.params.getValue().id;
-    //this.productId = route.snapshot.params['id'];
-    route.paramMap.subscribe(params => this.productId = params.get('id'));
+    this.productId = route.snapshot.params['id'];
+    //route.paramMap.subscribe(params => this.productId = params.get('id'));
+    this.productCategory = route.snapshot.queryParamMap.get('category');
   }
 
   ngOnInit() {
