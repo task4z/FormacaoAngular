cc95a43d58e656cb402705032c8755fd727932d2
unknown <andreafl90@gmail.com>
2019-02-18 19:39:44 +0000

    coisas meio feitas

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -8,9 +8,32 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
 @Input() article: Article;
+token: string;
 
   constructor() {
     this.article = new Article('asdasd', 'asdasd', 5);
+    this.token = new Date().toISOString().slice(5).replace('-', '').replace(/T/g, '').replace(/:/g, '').replace('.', '').replace(/Z/g, '');
+    const arr = [];
+
+    while ( arr.length < 2) {
+      const a = Math.floor(Math.random() * (this.token.length - 0) + 0);
+      if ( !arr.includes(a) ) {
+          arr.push(a);
+          this.token = this.token.substr(0, a)
+           + String.fromCharCode(this.token.charCodeAt(a) + Math.floor(Math.random() * (8 - 33) + 33))
+           + this.token.substr(a + 1);
+      }
+    }
+
+    while ( arr.length < 4) {
+      const a = Math.floor(Math.random() * (this.token.length - 0) + 0);
+      if ( !arr.includes(a) ) {
+          arr.push(a);
+          this.token = this.token.substr(0, a)
+          + String.fromCharCode(this.token.charCodeAt(a) + Math.floor(Math.random() * (65 - 40) + 40))
+          + this.token.substr(a + 1);
+      }
+    }
   }
 
   voteUp() {
